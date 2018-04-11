6c50558fc5d4c3c2dc5cdc6ca875439061b920b9
pisty2 <pisty2@gmail.com>
2018-04-11 15:52:44 +0200

    fix mongo + test http

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/comment-mongo');
+mongoose.connect('mongodb://mongo/comment-mongo');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
